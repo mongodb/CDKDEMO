703b18d5b859baaafa32384259e01bc3dff5d0ae
ialek36 <ialek36@gmail.com>
2023-03-28 11:42:00 -0400

    modified tests to use axios

--- mern/server/tests/api.integ.test.js
+++ mern/server/tests/api.integ.test.js
@@ -1,15 +1,25 @@
+const axios = require('axios');
+
+// test('getting total records record', async () =>{
+//     const response = await fetch(`http://localhost:5000/record/`)
+//     let records = await response.json()
+
+//     expect(records.length).toBe(3)
+// });
+
+// test('test specific records', async () =>{
+//     const response = await fetch(`http://localhost:5000/name/Peter`)
+//     let records = await response.json()
+
+//     expect(records.length).toBe(1)
+//     expect(records[0].level).toBe("Intern")
+// });
+
 test('getting total records record', async () =>{
-    const response = await fetch(`http://localhost:5000/record/`)
-    let records = await response.json()
+    const response = await axios(`http://localhost:5000/record/`)
+    let records = await response.data;
 
     expect(records.length).toBe(3)
 });
 
-test('test specific records', async () =>{
-    const response = await fetch(`http://localhost:5000/name/Peter`)
-    let records = await response.json()
-
-    expect(records.length).toBe(1)
-    expect(records[0].level).toBe("Intern")
-});
 
