270e1c478e27743c4352f17eb7fb056a1e79f7ed
ialek36 <ialek36@gmail.com>
2023-04-06 12:56:15 -0400

    restored cluster provisioning

--- atlas-integ-infra/lib/atlas-integ-infra-stack.ts
+++ atlas-integ-infra/lib/atlas-integ-infra-stack.ts
@@ -8,46 +8,46 @@ export class AtlasIntegInfraStack extends cdk.Stack {
     super(scope, id, props);
 
     const orgId = this.node.tryGetContext('MONGODB_ATLAS_ORG_ID') || process.env.MONGODB_ATLAS_ORG_ID;
-    // const replicationSpecs = [
-    //   {
-    //     "numShards": 1,
-    //     "advancedRegionConfigs": [
-    //       {
-    //         "analyticsSpecs": {
-    //           "ebsVolumeType": "STANDARD",
-    //           "instanceSize": "M10",
-    //           "nodeCount": 1
-    //         },
-    //         "electableSpecs": {
-    //           "ebsVolumeType": "STANDARD",
-    //           "instanceSize": "M10",
-    //           "nodeCount": 3
-    //         },
-    //         "priority": 7,
-    //         "regionName": "US_EAST_1"
-    //       }
-    //     ]
-    //   }
-    // ]
+    const replicationSpecs = [
+      {
+        "numShards": 1,
+        "advancedRegionConfigs": [
+          {
+            "analyticsSpecs": {
+              "ebsVolumeType": "STANDARD",
+              "instanceSize": "M10",
+              "nodeCount": 1
+            },
+            "electableSpecs": {
+              "ebsVolumeType": "STANDARD",
+              "instanceSize": "M10",
+              "nodeCount": 3
+            },
+            "priority": 7,
+            "regionName": "US_EAST_1"
+          }
+        ]
+      }
+    ]
 
-    // const atlas = new AtlasBasic(this, 'atlas-basic', {
-    //   clusterProps: {
-    //     replicationSpecs: replicationSpecs
-    //   },
-    //   projectProps: {
-    //     orgId: orgId,
-    //   },
-    //   ipAccessListProps: {
-    //     accessList: [
-    //       { ipAddress: '0.0.0.0/0', comment: 'My first IP address' }
-    //     ]
-    //   }
-    // });
+    const atlas = new AtlasBasic(this, 'atlas-basic', {
+      clusterProps: {
+        replicationSpecs: replicationSpecs
+      },
+      projectProps: {
+        orgId: orgId,
+      },
+      ipAccessListProps: {
+        accessList: [
+          { ipAddress: '0.0.0.0/0', comment: 'My first IP address' }
+        ]
+      }
+    });
 
-    // new cdk.CfnOutput(this, "atlas-url", {
-    //   value: (atlas.mCluster.props.connectionStrings ?? "UNDEFINED").toString(),
-    //   exportName: "atlas-url",
-    // });
+    new cdk.CfnOutput(this, "atlas-url", {
+      value: (atlas.mCluster.props.connectionStrings ?? "UNDEFINED").toString(),
+      exportName: "atlas-url",
+    });
 
     new cdk.CfnOutput(this, "hello-world", {
       value: "Hi there!",
